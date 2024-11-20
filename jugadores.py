f1ab3319e0b72f9923ed735429cdf8dda24bd47e
jonathanbello123 <jonathanbellogabriel@gmail.com>
2024-11-20 15:34:28 -0300

    juego terminado en consola

--- jugadores.py
+++ jugadores.py
@@ -1,19 +1,34 @@
 import json
+from datetime import datetime
+
+
+fecha = datetime.now()
+# año = fecha.strftime("%Y")
+# mes = fecha.strftime("%m")
+# dia=fecha.strftime("%d")
+# reloj=fecha.strftime("%H:%M:%S")
+fecha_completa = fecha.strftime("%d/%m/%Y, %H:%M:%S")
+
 
 def obtener_nombres_jugadores():
     datos_jugadores = {}
     for i in range(1, 3):
-        nombre = ""
+        nombre = input(f"Ingrese el nombre del jugador {i}: ")
         while len(nombre.strip()) == 0:
-            nombre = input(f"Ingrese el nombre del jugador {i}: ")
-            if len(nombre.strip()) == 0:
-                print("El nombre no puede estar vacio. Try again.")
-        datos_jugadores[f"jugador{i}"] = {"nombre": nombre.strip(), "puntuacion": 0}
+            nombre = input(
+                f"El campo esta vacio.Ingrese el nombre del jugador {i}: ")
+        datos_jugadores[f"jugador{i}"] = {
+            "nombre": nombre.strip(), "puntuacion": 0}
     return datos_jugadores
-    
+
 
 def guardar_datos_jugadores(datos_jugadores):
     archivo_json = "datos_jugadores.json"
+    datos_partida = {
+        "Fecha De partida": fecha_completa,
+        "Jugadores": datos_jugadores
+
+    }
 
     with open(archivo_json, "w") as archivo:
-        json.dump(datos_jugadores, archivo, indent=4)
+        json.dump(datos_partida, archivo, indent=4)
--- main.py
+++ main.py
@@ -2,56 +2,75 @@ from funciones import *
 from jugadores import obtener_nombres_jugadores, guardar_datos_jugadores
 from mazo import cargar_mazo, dividir_mazo
 from utilities import elegir_atributo_aleatorio, comparar_cartas
+import time
+
 
 def jugar_partida():
-    mazo = cargar_mazo("cartas.csv")
-    datos_jugadores = obtener_nombres_jugadores()
-    mazo_jugadores = dividir_mazo(mazo)
-    rondas = 1
-    mesas = []
-    max_rondas = 250
-    empate = False
-    victorias_elementos = {"jugador1": 0, "jugador2": 0}
-    while rondas <= max_rondas:
-        print(f"\nronda: {rondas}")
 
-        carta1 = mazo_jugadores["jugador1"].pop(0)
-        carta2 = mazo_jugadores["jugador2"].pop(0)
+    while True:
+        opcion = input("1:Jugar 2:Salir")
+        match opcion:
+            case "1":
+                mazo = cargar_mazo("cartas.csv")
+                datos_jugadores = obtener_nombres_jugadores()
+                mazo_jugadores = dividir_mazo(mazo)
+                rondas = 1
+                mesas = []
+                max_rondas = 250
+                empate = False
+                victorias_elementos = {"jugador1": 0, "jugador2": 0}
+                while rondas <= max_rondas:
+                    print(f"\nronda: {rondas}")
 
-        atributo_elegido = elegir_atributo_aleatorio()
-        print(f"Atributo elegido: {atributo_elegido}")
+                    carta1 = mazo_jugadores["jugador1"].pop(0)
+                    carta2 = mazo_jugadores["jugador2"].pop(0)
 
-        resultado = comparar_cartas(carta1, carta2, atributo_elegido)
+                    atributo_elegido = elegir_atributo_aleatorio()
+                    print(f"Atributo elegido: {atributo_elegido}")
 
-        print(f"La carta del jugador {datos_jugadores['jugador1']['nombre']} es: {carta1}")
-        print(f"La carta del jugador {datos_jugadores['jugador2']['nombre']} es: {carta2}")
+                    resultado = comparar_cartas(
+                        carta1, carta2, atributo_elegido)
 
-        ganador_ronda(resultado, carta1, carta2,datos_jugadores, mazo_jugadores, mesas, empate, victorias_elementos, atributo_elegido)
-        
-        if victorias_elementos["jugador1"] >= 10:
-            print(f"{datos_jugadores['jugador1']['nombre']} es el ganador por alcanzar 10 victorias elementales.")
-            #print(f"{datos_jugadores['jugador1']['nombre']} tiene {len(mazo_jugadores['jugador1'])} cartas.")
-            #print(f"{datos_jugadores['jugador2']['nombre']} tiene {len(mazo_jugadores['jugador2'])} cartas.")
-            break
-        elif victorias_elementos["jugador2"] >= 10:
-            print(f"{datos_jugadores['jugador2']['nombre']} es el ganador por alcanzar 10 victorias elementales.")
-            #print(f"{datos_jugadores['jugador1']['nombre']} tiene {len(mazo_jugadores['jugador1'])} cartas.")
-            #print(f"{datos_jugadores['jugador2']['nombre']} tiene {len(mazo_jugadores['jugador2'])} cartas.")
-            break
+                    print(f"La carta del jugador {
+                        datos_jugadores['jugador1']['nombre']} es: {carta1}")
+                    print(f"La carta del jugador {
+                        datos_jugadores['jugador2']['nombre']} es: {carta2}")
 
+                    ganador_ronda(resultado, carta1, carta2, datos_jugadores, mazo_jugadores,
+                                  mesas, empate, victorias_elementos, atributo_elegido)
 
-        ganador_por_cartas = verificar_ganador_por_cartas(mazo_jugadores, datos_jugadores)
-        if ganador_por_cartas:
-            print(f"{datos_jugadores[ganador_por_cartas]['nombre']} es el ganador porque se ha quedado con todas las cartas")
-            break  
+                    if victorias_elementos["jugador1"] >= 10:
+                        print(f"{datos_jugadores['jugador1']['nombre']
+                                 } es el ganador por alcanzar 10 victorias elementales.")
+                        # print(f"{datos_jugadores['jugador1']['nombre']} tiene {len(mazo_jugadores['jugador1'])} cartas.")
+                        # print(f"{datos_jugadores['jugador2']['nombre']} tiene {len(mazo_jugadores['jugador2'])} cartas.")
+                        break
+                    elif victorias_elementos["jugador2"] >= 10:
+                        print(f"{datos_jugadores['jugador2']['nombre']
+                                 } es el ganador por alcanzar 10 victorias elementales.")
+                        # print(f"{datos_jugadores['jugador1']['nombre']} tiene {len(mazo_jugadores['jugador1'])} cartas.")
+                        # print(f"{datos_jugadores['jugador2']['nombre']} tiene {len(mazo_jugadores['jugador2'])} cartas.")
+                        break
 
+                    ganador_por_cartas = verificar_ganador_por_cartas(
+                        mazo_jugadores, datos_jugadores)
+                    if ganador_por_cartas:
+                        print(f"{datos_jugadores[ganador_por_cartas]['nombre']
+                                 } es el ganador porque se ha quedado con todas las cartas")
+                        break
 
+                    rondas += 1
+                    time.sleep(2)
 
-        rondas += 1
+                    determinar_ganador_final(
+                        datos_jugadores, mazo_jugadores, victorias_elementos)
 
-        determinar_ganador_final(datos_jugadores, mazo_jugadores, victorias_elementos)
-
-        guardar_datos_jugadores(datos_jugadores)
+                    guardar_datos_jugadores(datos_jugadores)
+            case "2":
+                print("saliendo....")
+                break
+            case _:
+                print("invalido")
 
 
 jugar_partida()
